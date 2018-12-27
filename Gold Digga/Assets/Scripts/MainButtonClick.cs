076dbc158294e34340bfa91447dd252ef3ec6b1e
DESKTOP-ANE7Q38\User <arfr@live.ru>
2018-12-27 03:46:49 +0100

    First Attemos by videolessons

---
+++ Gold Digga/Assets/Scripts/MainButtonClick.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainButtonClick : MonoBehaviour {
+
+    public GameObject textBox;
+
+    public void ClickTheButton()
+    {
+        GlobalCookies.CookieCount += 1;
+    }
+
+
+}
---
+++ Gold Digga/Assets/Scripts/SellCookie.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SellCookie : MonoBehaviour {
+
+    public GameObject textBox;
+    public GameObject statusBox;
+
+    public void ClickTheButton()
+    {
+        if (GlobalCookies.CookieCount <= 0)
+        {
+            statusBox.GetComponent<Text>().text = "Not enough cookies";
+            statusBox.GetComponent<Animation>().Play("StatusAnim");
+        }
+        else {
+            GlobalCookies.CookieCount -= 1;
+            GlobalCash.CashCount += 1;
+        }
+        
+    }
+
+
+}
